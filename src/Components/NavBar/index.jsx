23f82b117ee902358088ab3db3270db1bbcf026e
markhov <marco.ac01@gmail.com>
2025-01-06 23:54:43 -0300

    chore: modify navbar

--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -2,9 +2,9 @@ import { NavLink } from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
+    <nav className="flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light">
+      <ul className="flex items-center gap-3">
+        <li className="font-semibold text-lg">
           <NavLink to='/'>Shopi</NavLink>
         </li>
         <li>
@@ -26,7 +26,7 @@ const Navbar = () => {
           <NavLink to='/others'>Others</NavLink>
         </li>
       </ul>
-      <ul>
+      <ul className="flex items-center gap-3">
         <li>
           marco.parfums@andesdecants.cl
         </li>
