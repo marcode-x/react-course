ab1fec2f198fcc2ae7b4b0e2082e8efc516a7337
markhov <marco.ac01@gmail.com>
2025-01-06 23:06:20 -0300

    feat: add Components - Navbar

---
+++ src/Components/NavBar/index.jsx
@@ -0,0 +1,56 @@
+import { NavLink } from "react-router-dom"
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to='/'>Shopi</NavLink>
+        </li>
+        <li>
+          <NavLink to='/'>All</NavLink>
+        </li>
+        <li>
+          <NavLink to='/clothes'>Clothes</NavLink>
+        </li>
+        <li>
+          <NavLink to='/electronics'>Electronics</NavLink>
+        </li>
+        <li>
+          <NavLink to='/furnitures'>Furnitures</NavLink>
+        </li>
+        <li>
+          <NavLink to='/toys'>Toys</NavLink>
+        </li>
+        <li>
+          <NavLink to='/others'>Others</NavLink>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          marco.parfums@andesdecants.cl
+        </li>
+        <li>
+          <NavLink to='/my-orders'>
+            My Orders
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/my-account'>
+            My Account
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/sign-in'>
+            Sign In
+          </NavLink>
+        </li>
+        <li>
+          Icon
+        </li>
+
+      </ul>
+
+    </nav>
+  )
+}
