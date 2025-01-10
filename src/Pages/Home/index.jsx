913a9faddca5bd731a17b2626252577ed0ba3986
markhov <marco.ac01@gmail.com>
2025-01-10 01:40:01 -0300

    feat: add API for Cards

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,24 @@
+import { useState, useEffect } from "react";
 import Layout from "../../Components/Layout"
 import Card from "../../Components/Card"
 
 function Home() {
+  const [items, setItems] = useState(null)
+
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then(response => response.json())
+      .then(data => setItems(data))
+  }, [])
+
   return (
     <Layout>
       Homex
+      {
+        items?.map(() => {
+          return <Card />
+        })
+      }
       <Card />
     </Layout>
   )
