1d2a23f0a53fda96794730c770154682ce46b69a
markhov <marco.ac01@gmail.com>
2025-01-11 01:04:47 -0300

    feat: add Product Detail

---
+++ src/Components/ProductDetail/index.jsx
@@ -0,0 +1,17 @@
+import "./styles.css"
+
+const ProductDetail = () => {
+  return (
+    <aside className="product-detail flex flex-col fixed right-0 border border-black bg-white rounded-lg">
+      <div className="flex justify-between items-center p-6">
+        <h2 className="font-medium text-xl">Detail</h2>
+        <div>
+          x
+        </div>
+      </div>
+
+    </aside>
+  )
+}
+
+export default ProductDetail
