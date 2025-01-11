8e47b179a2b9945b8a0609088bc72fd818f793af
markhov <marco.ac01@gmail.com>
2025-01-11 02:24:47 -0300

    fix: modify images array

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -22,7 +22,7 @@ const ProductDetail = () => {
       <figure className='px-6'>
         <img
           className="w-full  h-full rounded-lg"
-          src={context.productToShow.images[0]}
+          src={context.productToShow.images?.[0]}
           alt={context.productToShow.title} />
       </figure>
       <p className='flex flex-col p-6'>
@@ -30,7 +30,6 @@ const ProductDetail = () => {
         <span className='font-medium text-md'>{context.productToShow.title}</span>
         <span className='font-light text-sm'>{context.productToShow.description}</span>
       </p>
-
     </aside>
   )
 }
