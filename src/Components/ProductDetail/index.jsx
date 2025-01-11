54d5ef6d2d1f214a460b3491f498f9d23033df3a
markhov <marco.ac01@gmail.com>
2025-01-11 01:41:25 -0300

    feat: add show and hide product detail

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,13 +1,21 @@
 import { XMarkIcon } from '@heroicons/react/24/solid'
 import "./styles.css"
+import { useContext } from 'react'
+import { ShoppingCartContext } from '../../Context'
 
 const ProductDetail = () => {
+  const context = useContext(ShoppingCartContext)
+
   return (
-    <aside className="product-detail flex flex-col fixed right-0 border border-black bg-white rounded-lg">
+    <aside
+      className={`${context.isProductDetailOpen ? "flex" : "hidden"} product-detail flex-col fixed right-0 border border-black bg-white rounded-lg`}>
       <div className="flex justify-between items-center p-6">
         <h2 className="font-medium text-xl">Detail</h2>
         <div>
-          <XMarkIcon className="size-6 text-black"></XMarkIcon>
+          <XMarkIcon
+            className="size-6 text-black"
+            onClick={() => context.closeProductDetail()}>
+          </XMarkIcon>
         </div>
       </div>
 
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -6,13 +6,20 @@ export const ShoppingCartProvider = ({ children }) => {
 
   // Convert Local state in global state
   const [count, setCount] = useState(0)
+  const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
   // console.log("COUNT: ", count)
+  const openProductDetail = () => setIsProductDetailOpen(true)
+  const closeProductDetail = () => setIsProductDetailOpen(false)
+
 
 
   return (
     <ShoppingCartContext.Provider value={{
       count,
-      setCount
+      setCount,
+      openProductDetail,
+      closeProductDetail,
+      isProductDetailOpen
     }}>
       {children}
     </ShoppingCartContext.Provider>
