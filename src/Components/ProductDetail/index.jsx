3e89aafdf18b00c6ca1e6aa8752d7d367f84c245
markhov <marco.ac01@gmail.com>
2025-01-12 17:11:12 -0300

    chore: add scroll to Product Detail

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -9,7 +9,7 @@ const ProductDetail = () => {
 
   return (
     <aside
-      className={`${context.isProductDetailOpen ? "flex" : "hidden"} product-detail flex-col fixed right-0 border border-black bg-white rounded-lg`}>
+      className={`${context.isProductDetailOpen ? "flex" : "hidden"} overflow-y-scroll product-detail flex-col fixed right-0 border border-black bg-white rounded-lg`}>
       <div className="flex justify-between items-center p-6">
         <h2 className="font-medium text-xl">Detail</h2>
         <div>
