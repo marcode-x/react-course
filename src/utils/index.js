6e185ff8ab8f707f384f268e0e4130b06dc03f80
markhov <marco.ac01@gmail.com>
2025-01-13 01:27:22 -0300

    chore: modify totalPrice function using reduce

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,8 +4,4 @@
  * @returns {number} Total Price
 */
 
-export const totalPrice = (products) => {
-  let sum = 0
-  products.forEach(product => sum += product.price)
-  return sum
-}
+export const totalPrice = (products) => (products.reduce((sum, product) => sum + product.price, 0));
