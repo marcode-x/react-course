907d4a8202c302a4c42e89f4ea2b8cdc00df7b2e
markhov <marco.ac01@gmail.com>
2025-01-14 00:46:08 -0300

    chore: edit my orders page

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -1,18 +1,24 @@
 import { XMarkIcon } from '@heroicons/react/24/solid'
+import { ArrowTopRightOnSquareIcon } from '@heroicons/react/24/solid'
+import { CalendarDaysIcon } from '@heroicons/react/24/solid'
 
 const OrdersCard = props => {
   const { totalPrice, totalProducts } = props
 
   return (
-    <div className="flex justify-between items-center mb-2.5 ">
-      <p>
-        <span>01.02.25</span>
-        <span>{totalProducts}</span>
-        <span>{totalPrice}</span>
-      </p>
-    </div>
+    <div className="flex justify-between items-center mb-2.5 border border-black w-80 p-4 rounded-lg">
+      <div className='flex justify-between w-full'>
+        <p className='flex items-center justify-between'>
+          <span className='font-light'>01.02.25</span>
+          <span className='font-light'>{totalProducts} Articles</span>
+        </p>
+        <p className='flex items-center gap-2'>
+          <span className='font-medium text-2xl'>${totalPrice}</span>
+          <ArrowTopRightOnSquareIcon className='h-6 w-6 text-black cursor-pointer' />
+        </p>
+      </div>
+    </div >
   )
 }
-
 
 export default OrdersCard
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -10,8 +10,8 @@ function MyOrders() {
 
   return (
     <Layout>
-      <div className="flex items-center justify-center relative w-80">
-        <h1>My Orders</h1>
+      <div className="flex items-center justify-center relative w-80 mb-4">
+        <h1 className="font-medium text-xl" >My Orders</h1>
       </div>
 
       {
