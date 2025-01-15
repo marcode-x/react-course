841d16129810ae5245098cf510cd0e72e7f09b26
markhov <marco.ac01@gmail.com>
2025-01-15 01:32:41 -0300

    feat: add product search

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react"
+import { createContext, useState, useEffect } from "react"
 
 export const ShoppingCartContext = createContext()
 
@@ -33,6 +33,19 @@ export const ShoppingCartProvider = ({ children }) => {
   // Shopping Cart - Order
   const [order, setOrder] = useState([])
 
+  // Get products 
+  const [items, setItems] = useState(null)
+
+  // Get products by Title
+  const [searchByTitle, setSearchByTitle] = useState(null)
+  console.log("searchByTitle: ", searchByTitle)
+
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then(response => response.json())
+      .then(data => setItems(data))
+  }, [])
+
 
 
   return (
@@ -55,7 +68,11 @@ export const ShoppingCartProvider = ({ children }) => {
       openOrderCard,
       closeOrderCard,
       order,
-      setOrder
+      setOrder,
+      items,
+      setItems,
+      searchByTitle,
+      setSearchByTitle
     }}>
       {children}
     </ShoppingCartContext.Provider>
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,23 +1,27 @@
-import { useState, useEffect } from "react";
+import { useContext } from "react";
 import Layout from "../../Components/Layout"
 import Card from "../../Components/Card"
 import ProductDetail from "../../Components/ProductDetail"
+import { ShoppingCartContext } from "../../Context";
 
 function Home() {
-  const [items, setItems] = useState(null)
-
-  useEffect(() => {
-    fetch("https://api.escuelajs.co/api/v1/products")
-      .then(response => response.json())
-      .then(data => setItems(data))
-  }, [])
+  const context = useContext(ShoppingCartContext)
 
   return (
     <Layout>
-      Home
+      <div className="flex items-center justify-center relative w-80 mb-4">
+        <h1 className="font-medium text-xl">Exclusive Products</h1>
+      </div>
+      <input
+        className="rounded-lg border border-black w-80 p-4 mb-4 focus:outline-none"
+        type="text"
+        placeholder="Search a product"
+        onChange={(event) => context.setSearchByTitle(event.target.value)}
+      />
+
       <div className="grid gap-3 grid-cols-3 w-full max-w-screen-lg">
         {
-          items?.map(item => (
+          context.items?.map(item => (
             <Card key={item.id} data={item} />
           ))
         }
