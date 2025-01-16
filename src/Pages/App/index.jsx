b2bea87da344dc34f0e71a41d35b9def1a0c9ecc
markhov <marco.ac01@gmail.com>
2025-01-16 02:05:29 -0300

    fix: modify repo route

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -13,7 +13,7 @@ import "./App.css";
 const AppRoutes = () => {
   let routes = useRoutes([
     { path: '/', element: <Home /> },
-    { path: '/react-course', element: <Home /> },
+    { path: '/react-course/', element: <Home /> },
     { path: '/clothes', element: <Home /> },
     { path: '/electronics', element: <Home /> },
     { path: '/furnitures', element: <Home /> },
